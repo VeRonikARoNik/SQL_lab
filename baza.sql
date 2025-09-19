e6b605029615317a10b188eec70c044e53b79029
veronika_hordieieva <76017554+VeRonikARoNik@users.noreply.github.com>
2025-09-19 08:55:18 +0200

    Add files via upload

--- baza.sql
+++ baza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Sty 2022, 20:00
+-- Czas generowania: 27 Sie 2021, 18:45
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.2
 
@@ -19,141 +19,74 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `piekarnia`
+-- Baza danych: `konkurs`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `wyroby`
+-- Struktura tabeli dla tabeli `nagrody`
 --
 
-CREATE TABLE `wyroby` (
-  `ID` int(11) NOT NULL,
-  `Rodzaj` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `Nazwa` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `Gramatura` float NOT NULL,
-  `Cena` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+CREATE TABLE `nagrody` (
+  `id_nagrody` int(11) NOT NULL,
+  `nazwa` varchar(25) COLLATE utf8mb4_polish_ci NOT NULL,
+  `opis` tinytext COLLATE utf8mb4_polish_ci NOT NULL,
+  `cena` decimal(19,2) DEFAULT NULL,
+  `ilosc_sztuk` int(11) NOT NULL,
+  `data_dodania` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
--- Zrzut danych tabeli `wyroby`
+-- Zrzut danych tabeli `nagrody`
 --
 
-INSERT INTO `wyroby` (`ID`, `Rodzaj`, `Nazwa`, `Gramatura`, `Cena`) VALUES
-(1, 'INNE', 'Bułka tarta', 0.5, 2),
-(2, 'INNE', 'Pieczywo okolicznościowe np. \"Wiejski Stół\"', 1, 35),
-(3, 'INNE', 'Paszteciki z kapustą i grzybami', 0.2, 4.65),
-(4, 'PIECZYWO DLA GASTRONOMII', 'Bułka do Hamburgera', 0.12, 1.8),
-(5, 'PIECZYWO DLA GASTRONOMII', 'Bułka do HotDoga', 0.1, 2.8),
-(6, 'PIECZYWO DLA GASTRONOMII', 'Bułka do zapiekanki 30cm', 0.2, 3.8),
-(7, 'PIECZYWO DLA GASTRONOMII', 'Bułka do zapiekanki 40cm', 0.25, 4.8),
-(8, 'PIECZYWO DLA GASTRONOMII', 'Bułka do zapiekanki 50cm', 0.3, 5.8),
-(9, 'PIECZYWO MIESZANE', 'Chleb baltonowski', 0.6, 3.45),
-(10, 'PIECZYWO MIESZANE', 'Chleb duży wiejski', 1, 5.05),
-(11, 'PIECZYWO MIESZANE', 'Chleb baltonowski krojony', 0.6, 3.45),
-(12, 'PIECZYWO MIESZANE', 'Chleb z soją i słonecznikiem', 0.5, 3.05),
-(13, 'PIECZYWO MIESZANE', 'Chleb wieloziarnisty \"Słońce\"', 0.4, 2.65),
-(14, 'PIECZYWO MIESZANE', 'Chleb razowy 4 ziarna', 0.5, 3.05),
-(15, 'PIECZYWO MIESZANE', 'Chleb mieszany 4 ziarna', 0.6, 3.45),
-(16, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb oliwski', 0.5, 3.05),
-(17, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb oliwski krojony', 0.5, 3.05),
-(18, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb oliwski foremkowy', 0.5, 3.05),
-(19, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb wiejski', 0.6, 3.45),
-(20, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb wiejski duży', 1, 5.05),
-(21, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb rozmaitości', 0.5, 3.05),
-(22, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb oliwski z makiem', 0.5, 3.05),
-(23, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb oliwski z sezamem', 0.5, 3.05),
-(24, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb litewski', 0.5, 3.05),
-(25, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Chleb marmurkowy', 0.5, 3.05),
-(26, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Bułka razowa', 0.1, 1.45),
-(27, 'PIECZYWO MIESZANE (PSZENNO-ŻYTNIE)', 'Bułka obfitości', 0.5, 3.05),
-(28, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Bułka nadziewana i Pączek', 0.08, 1.37),
-(29, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Bułka turecka \"Żulik\"', 0.2, 1.85),
-(30, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Rogalik ECO z serem i oregano', 0.07, 1.33),
-(31, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Bulka PIZZA', 0.1, 1.45),
-(32, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Rogalik z kapustą i grzybami', 0.12, 1.53),
-(33, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Zapiekanka z pieczarkami i serem', 0.12, 1.53),
-(34, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Bułka włoska CIABATTA', 0.06, 1.29),
-(35, 'PIECZYWO PÓŁCUKIERNICZE I FIRMOWE', 'Chleb ORKISZOWY', 0.3, 2.25),
-(36, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Chleb pszenny', 0.4, 2.65),
-(37, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka mini', 0.03, 1.17),
-(38, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka paryska', 0.4, 2.65),
-(39, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka szwedka', 0.05, 1.25),
-(40, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka wrocławska', 0.05, 1.25),
-(41, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka kajzerka', 0.05, 1.25),
-(42, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka z serem', 0.1, 1.45),
-(43, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bułka rozmaitości', 0.1, 1.45),
-(44, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bagietka mała', 0.15, 1.65),
-(45, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Bagietka duża', 0.25, 2.05),
-(46, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Rogal duży', 0.4, 2.65),
-(47, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Rogal zwykły', 0.1, 1.45),
-(48, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Rogal maślany', 0.1, 1.45),
-(49, 'PIECZYWO PSZENNE (BUŁKOWE)', 'Solanka', 0.1, 1.45),
-(50, 'PIECZYWO PSZENNE (CHLEBOWE)', 'Chleb graham', 0.5, 3.05),
-(51, 'PIECZYWO PSZENNE (CHLEBOWE)', 'Bułka grahamka', 0.1, 1.45),
-(52, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Bułka wrocławska', 0.4, 2.65),
-(53, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Bułka z przedziałkiem', 0.09, 1.41),
-(54, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Bułka \"Kajzerka\"', 0.05, 1.25),
-(55, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Bułka grahamka duża', 0.09, 1.41),
-(56, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Bułka grahamka mała', 0.05, 1.25),
-(57, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Chałka duża', 0.5, 4.05),
-(58, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Chałka mała', 0.25, 3.05),
-(59, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Rogal maślany', 0.09, 2.41),
-(60, 'PIECZYWO PSZENNE WYBOROWE I MAŚLANE', 'Chleb tostowy', 0.35, 3.45),
-(61, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Chałka pleciona', 0.3, 3.25),
-(62, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Chałka foremkowa', 0.3, 3.25),
-(63, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Bułka maślana z makiem', 0.09, 2.41),
-(64, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Bułka maślana z kruszonką', 0.09, 2.41),
-(65, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Plecionka maślana', 0.09, 2.41),
-(66, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Paluch maślany', 0.09, 2.41),
-(67, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Drożdżówka z kruszonką', 0.1, 2.45),
-(68, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Drożdżówka z makiem', 0.1, 2.45),
-(69, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Drożdżówka z serem', 0.1, 2.45),
-(70, 'PIECZYWO WYBOROWE I PÓŁCUKIERNICZE', 'Drożdżówka ósemka', 0.1, 2.45),
-(71, 'PIECZYWO ŻYTNIE', 'Chleb razowy', 0.5, 4.05),
-(72, 'PIECZYWO ŻYTNIE', 'Chleb wiejski żytni duży', 2, 21),
-(73, 'PIECZYWO ŻYTNIE', 'Chleb wiejski żytni średni', 1, 12),
-(74, 'PIECZYWO ŻYTNIE', 'Chleb razowy', 0.5, 4.05),
-(75, 'PIECZYWO ŻYTNIE', 'Chleb razowy turystyczny', 0.5, 4.05),
-(76, 'PIECZYWO ŻYTNIE', 'Chleb razowy słonecznikowy', 0.5, 4.05),
-(77, 'PIECZYWO ŻYTNIE', 'Chleb razowy ze śliwką', 0.5, 4.05),
-(78, 'PIECZYWO ŻYTNIE', 'Chleb razowy na miodzie', 0.5, 4.05),
-(79, 'PIECZYWO ŻYTNIE', 'Chleb razowy z rodzynkami', 0.5, 4.05),
-(80, 'PIECZYWO ŻYTNIE', 'Chleb fiński', 0.5, 4.05),
-(81, 'PIECZYWO ŻYTNIE', 'Chleb żytni mały', 0.5, 4.05),
-(82, 'PIECZYWO ŻYTNIE', 'Chleb żytni duży', 0.5, 4.05),
-(83, 'PIECZYWO ŻYTNIE', 'Chleb żytni z pestką dyni', 0.5, 4.05),
-(84, 'PIECZYWO ŻYTNIE', 'Chleb żytni ze słonecznikiem', 0.5, 4.05),
-(85, 'PIECZYWO ŻYTNIE', 'Chleb żytni z cebulą', 0.5, 4.05),
-(86, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek', 0.1, 2.45),
-(87, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek z jabłkiem', 0.1, 2.45),
-(88, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek z toffi', 0.1, 2.45),
-(89, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek z adwokatem', 0.1, 2.45),
-(90, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Ciastko francuskie', 0.1, 2.45),
-(91, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Ciastko półfrancuskie', 0.1, 2.45),
-(92, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek', 0.1, 2.45),
-(93, 'WYROBY CUKIERNICZE I CIASTKARSKIE', 'Pączek', 0.1, 2.45);
+INSERT INTO `nagrody` (`id_nagrody`, `nazwa`, `opis`, `cena`, `ilosc_sztuk`, `data_dodania`) VALUES
+(1, 'Laptop EXTRIME EDU', 'Laptop EXTRIME EDU został wyposażony w wydajny sześciordzeniowy procesor AMD Ryzen 7 4800H. Dlatego sprawdzi się on bardzo dobrze podczas normalnego korzystania, ale także jako narzędzie to rozgrywania emocjonujących rywalizacji w świecie gier.', '4599.00', 1, '2021-08-03'),
+(2, 'Konsola EDU PRO', 'MAŁA KONSOLA, DUŻA MOC. Używaj tylko z grami cyfrowymi - konsola nie odtwarza dysków fizycznych. EDU PRO to najmniejsza konsola w historii. Ta całkowicie cyfrowa konsola zachwyci Cię nie tylko swoją szybkością ale i wydajnością.', '1138.50', 3, '2021-08-19'),
+(3, 'Smartfon OKKO 10 PRO', 'OKKO 10 PRO 256 GB (CZARNY) Designerski smartfon w szklanej obudowie 3D, wyposażony w 16-rdzeniowy procesor Helio G90T, poczwórny aparat 128 Mpix, pełnoekranowy wyświetlacz AMOLED+ i moduł NFC.', '1828.00', 4, '2021-07-21'),
+(4, 'Smart watch LEARNING', 'LEARNING GTXX (ZŁOTY) Elegancki i ultrasmukły smartwatch z udoskonaloną baterią o zwiększonej wydajności, superszybkim procesorem Kirin A1 oraz bogatą gamą inteligentnych funkcji.', '1230.00', 2, '2021-07-22'),
+(5, 'Tablet SPORT Tab FFF 5G', 'DAJ SIĘ UWIEŚĆ DŹWIĘKOM I OBRAZOM SPORT Tab FFF 5G posiada niezwykle żywy 12,4-calowy ekran, który uwypukli każdy szczegół. Podwójne głośniki i dźwięk AKG zadbają o niesamowite kinowe doznania. ', '1673.00', 2, '2021-06-23'),
+(6, 'Słuchawki BIG 6 ', 'Słuchawki BIG 6 mają wszystko, czego potrzebujesz, by zanurzyć się w głębokim i bogatym dźwięku. Zaawansowane rozwiązania, takie jak dwudrożne głośniki, ulepszona transmisja głosu czy aktywna redukcja szumów.', '650.00', 4, '2021-08-21'),
+(7, 'Głośnik BIASS 3000', 'Bezprzewodowy głośnik multiroom (jasnoszary) inteligentny głośnik z dźwiękiem 360.', '460.00', 2, '2021-06-14'),
+(8, 'Pendrive ULTRA ', 'ULTRA USB 3.0 256GB pozwoli Ci mieć zawsze przy sobie ulubione zdjęcia i potrzebne dokumenty cyfrowe. Poręczny pendrive zmieści się w kieszeni i możesz doczepić go do kluczy.', '222.22', 1, '2021-08-08'),
+(9, 'Książka BESTSEL', 'Sofokles, Antygona – tragedia Sofoklesa z roku 442 p.n.e. Nazywana jest tragedią władzy i klasyfikowana jako chronologicznie ostatnia z cyklu trzech tragedii tebańskich opartych na mitach o tebańskim rodzie Labdakidów.', '12.10', 7, '2021-07-10'),
+(10, 'Długopis LINE', 'Długopis LINE 4 kolory - GRAWER Ikona marki LINE. Dedykowany dla osób, które zawsze noszą długopis przy sobie. Dołączono pudełko upominkowe. Dostarczany z opatentowanym wkładem SUPERinkFlow.', '13.75', 7, '2021-06-12'),
+(11, 'Piórnik szkolny', 'Piórnik szkolny z wyposażeniem MEGA GAME. Z przodu grafika z motywem gry. Dedykowany jest dla fana gier komputerowych. Piórnik idealnie pasuje do innych produktów z tej serii.', '173.00', 9, '2021-06-13'),
+(12, 'Komplet zeszytów', 'Zeszyty PAPIER PP cechują się wysoką jakością papieru, estetyką wykończenia, modną grafiką okładek i hybrydowym wykończeniem. Wysokiej jakości uszlachetniona okładka PP. W zeszytach znajdziesz \"pigułkę wiedzy\".', '35.28', 8, '2021-08-01'),
+(13, 'Mysz Wireless PPRO', 'Wireless PPRO to wyjątkowa mysz go grania dla profesjonalnych e-sportowców. Przez ponad 3 lata dział firmy współpracował z ponad 150 profesjonalnymi graczami, aby móc zagwarantować idealny kształt, masę i odczucia.', '728.00', 5, '2021-08-31'),
+(14, 'Słuchawki EEAR', 'Słuchawki do gier oferują doskonałą jakość dźwięku i mocny bas. Miękkie nauszniki i regulowany pałąk sprawiają, że słuchawki wygodnie leżą nawet podczas długich godzin gry. Głośność jest łatwa do kontrolowania dzięki regulacji.', '127.18', 2, '2021-07-31'),
+(15, 'Kakao OKO', 'Kakao ekologiczne, niski indeks glikemiczny, niskotłuszczowe, odpowiednie dla diety ketogenicznej, raw (surowe), superfood, wegańskie, wegetariańskie, wysoka zawartość błonnika, wysokobiałkowe.', '29.00', 3, '2021-06-07'),
+(16, 'Lusterko LED', 'Urocze lusterko kosmetyczne z podświetleniem LED. Produkt wyposażony jest w kabel USB dzięki któremu możemy naładować lusterko. Posiada również podstawkę, która może posłużyć jako organizer na drobne akcesoria.', '18.80', 5, '2021-07-10'),
+(17, 'Klamerki do prania', 'Pamiętasz, jak jako dziecko uwielbiałeś zaglądać do pralni i zabierać z kosza klamerki na pranie? Wtedy traktowałeś je jako fajne zabawki, które można wykorzystać na wiele różnych sposobów. Teraz dorosłeś a spinacze dalej są Ci potrzebne.', '9.90', 10, '2021-08-23'),
+(18, 'Dętka MTB 26/1.75-2.5', 'Różnorodność dętek pozwala na właściwy wybór: dętka standardowa jest dostępna we wszystkich rozmiarach, oferuje najlepszy kompromis pomiędzy codzienną jazdą, a wagą, podczas gdy w wersjach Light największe znaczenie ma waga i osiągi.', '32.00', 2, '2021-08-17'),
+(19, 'Szklana rybka Nemo', 'Wykonana jest ręcznie ze szkła. Może stanowić oryginalną i niebanalną dekorację. Ozdobi i doda uroku każdemu wnętrzu. Idealna jako piękny, oryginalny prezent dla każdego miłośnika szklanych figurek oraz dla zodiakalnych ryb.', '48.00', 4, '2021-08-06'),
+(20, 'Zatyczki do uszu', 'Wkładki przeciwhałasowe wykonane z miękkiej hipoalergicznej pianki poliuretanowej, aby zapewnić pełny komfort i niewielki nacisk na wnętrze ucha. Ich gładka, odporna na zabrudzenia powierzchnia zapewnia lepszą higienę, trwałość i komfort.', '0.00', 9, '2021-08-02'),
+(21, 'Skarpety ALPAKA', 'Alpaki to ładne puszyste zwierzęta z rodziny wielbłądowatych żyjące głównie w Ameryce Południowej. Włókno alpaki jest niezwykle cienkie, ma wspaniałe właściwości termoizolacyjne, grzeje dużo lepiej niż wełna owcza.', NULL, 5, '2021-08-15'),
+(22, 'Maszynka MEAT', 'Duża moc pozwala zmielić aż do 1,7 kg mięsa w minutę. Trwała metalowa komora mielenia oraz nożyk są gwarancją długiego, bezproblemowego użytkowania.', NULL, 1, '2021-08-24'),
+(23, 'Żelazko SLIDER', 'Żelazko parowe SLIDER doskonale sprawdzi się w każdym gospodarstwie domowym. Wyposażone jest w takie funkcje jak wyrzut pary czy zabezpieczenie przed kapaniem podczas prasowania na niższych temperaturach.', NULL, 3, '2021-08-25'),
+(24, 'Puzzle 1000 Zgierz', 'Produkt został przygotowany przez znaną i lubianą markę Puzzleoni. Puzzle składają się z 1000 elementów i przedstawiają wspaniały krajobraz Zgierza. Puzzle z pewnością spodobają się fanom układanek.', NULL, 8, '2021-07-10'),
+(25, 'Smar do klarnetu', 'Smar nowy w sztyfcie do korków łączących poszczególne elementy klarnetu może być używany również do korków ustnikowych w saksofonach. Sztyft bezbarwny.', '46.00', 3, '2021-08-05');
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indeksy dla tabeli `wyroby`
+-- Indeksy dla tabeli `nagrody`
 --
-ALTER TABLE `wyroby`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE `nagrody`
+  ADD PRIMARY KEY (`id_nagrody`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `wyroby`
+-- AUTO_INCREMENT dla tabeli `nagrody`
 --
-ALTER TABLE `wyroby`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+ALTER TABLE `nagrody`
+  MODIFY `id_nagrody` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
